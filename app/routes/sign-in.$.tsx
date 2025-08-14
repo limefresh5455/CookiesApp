283df54fbc23654e0fe68e07a7046177407e4009
limefresh5455 <limefresh5455@gmail.com>
2025-08-14 18:19:06 +0530

    changes env

--- app/routes/sign-in.$.tsx
+++ app/routes/sign-in.$.tsx
@@ -1,11 +1,10 @@
 import {  useLoaderData } from "@remix-run/react";
-
 import type {  LoaderFunction } from '@remix-run/node';
 import { rootAuthLoader } from '@clerk/remix/ssr.server';
-
-
 import { ClerkApp, SignIn } from "@clerk/remix";
-
+import {
+  Page,
+} from "@shopify/polaris";
 
 
 export const loader: LoaderFunction = (args) => rootAuthLoader(args);
@@ -14,14 +13,16 @@ export function App() {
   const { showForm } = useLoaderData<typeof loader>();
 
   return (
+      <Page title="Sign Up" fullWidth>
       <div style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-      }}>
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+       }}> 
       <SignIn path='/sign-in' signUpUrl='/sign-up' routing="path" />
       </div>
+      </Page>
   );
 }
 
--- app/routes/sign-up.$.tsx
+++ app/routes/sign-up.$.tsx
@@ -1,26 +1,24 @@
-
-
 import type { LoaderFunction } from '@remix-run/node';
 import { rootAuthLoader } from '@clerk/remix/ssr.server';
-
-
 import { ClerkApp, SignUp } from "@clerk/remix";
-
-
-
+import {
+  Page,
+} from "@shopify/polaris";
 export const loader: LoaderFunction = (args) => rootAuthLoader(args);
 
 export function App() {
 
   return (
-    <div style={{
+    <Page title="Sign Up" fullWidth>
+      <div style={{
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
       height: '100vh',
-    }}>
-      <SignUp path='/sign-up' signInUrl='/sign-in' routing="path" />
-    </div>
+       }}>  
+      </div>
+      <SignUp path='/sign-up' signInUrl='/sign-in' routing="path" />  
+    </Page>
   );
 }
 
