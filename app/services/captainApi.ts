db17d50049b3f4031b40babab4f0947f25caaf7a
limefresh5455 <limefresh5455@gmail.com>
2025-08-14 14:38:36 +0530

    changes fetchshopdata file and captainapi file

--- app/services/captainApi.ts
+++ app/services/captainApi.ts
@@ -152,7 +152,7 @@ export async function viewCounts(userId: string, scannerId: string, from: string
 
 export async function statusCounts(userId: string, scannerId: string, from: string, to: string){
   try {
-    const apiUrl = `${appdomain}/bannerTracking/statusCounts?from=${from}&to=${to}&scannerId=${scannerId}&userId=${userId}`;
+    const apiUrl = `${process.env.VITE_API_URL}/bannerTracking/statusCounts?from=${from}&to=${to}&scannerId=${scannerId}&userId=${userId}`;
     console.log("viewCounts API URL:", apiUrl); // Debug
     const response = await fetch(apiUrl);
     if (!response.ok) {
